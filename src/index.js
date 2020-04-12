c3f05193ebb24f7882774e37b2692f4b8ae384e8
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-04-13 00:49:02 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "react-redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/rootReducer";
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer);
+
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>
 );
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
