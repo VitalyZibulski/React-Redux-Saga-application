e0d4a97205eee0663d8f3b9c056223120397be2d
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-04-18 19:58:56 +0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { rootReducer } from "./redux/rootReducer";
 import thunk from "redux-thunk";
+import { forbiddenWordsMiddleware } from "./redux/middleware";
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, forbiddenWordsMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
